bbd1f4e651f4d4e2425fe4c1f732b14d25b29fb4
Shrinathv67 <158308168+Shrinathv67@users.noreply.github.com>
2025-01-18 21:09:49 -0600

    Calculate Income Tax

---
+++ tax.py
@@ -0,0 +1,16 @@
+"""
+Prompt the user to enter the annual income. 
+All income above 500000 is taxed at 10%.
+Print the tax.
+
+"""
+
+ann_income = input("Please enter the annual income: ")
+tax_income = ann_income - 500_000
+if tax_income > 0:
+    tax = tax_income*(10/100)
+    print(tax)
+else:
+    print("No tax")
+    
+
