eab968b24ed7eae28a8f59167449c4b0d558d06f
Shrinathv67 <158308168+Shrinathv67@users.noreply.github.com>
2025-01-29 16:15:54 -0600

    Create hello.py

---
+++ S03_Exercise/hello.py
@@ -0,0 +1,30 @@
+def get_username():
+    """ 
+    Prompt the user to enter his name
+    Input : None
+    Output : Return the user input as a string
+    """
+    # Your solution code should go in here
+    name = input("Please enter your name: ")
+    
+
+def say_hello(user):
+    '''
+    Greet the user by saying Hello followed by his name
+    Input : user name
+    Output : Greeting statement on the screen. No return value
+    Example : If input string is Sam
+        Then the expected output is
+        Hello Sam !!!
+    '''
+    print("Hello", user, "!!!")
+
+def main():
+    '''
+    Main function of the program
+    '''
+    name = get_username()
+    say_hello(name)
+    
+# Main starts from below
+main()
