7a34d4d2d62f7c1f901b3ed0c9a8501b44aecec4
Shrinathv67 <158308168+Shrinathv67@users.noreply.github.com>
2025-03-25 22:44:56 -0500

    Control Structures Exercises
    Changes to the Control Structure exercises

--- ControlStructures_Exercises/S05Q02.py
+++ ControlStructures_Exercises/S05Q02.py
@@ -1,14 +1,35 @@
 """Program to print the max and min of the numbers entered by the user until the user enters zero."""
 
 def get_usrinput():
-   num = 0
-   while num!=0:
-       num = input("Please enter the number")
-   else:
-       return "The user has input 0."
- 
+   num = int(input("Please enter a number:"))
+   num_list = []
+   while num != 0:
+       num_list.append(num)
+       num = int(input("Please enter a number:"))
+   print("The user has entered a zero.")
+   return num_list    
+
+def do_max_min_num(num_list):
+    num_list.sort()
+    print(f"The Min number from the user entered numbers is :{num_list[0]}")
+    print(f"The Max number from the user entered numbers is :{num_list[-1]}")
+    
+def do_digit_check(num_list):
+    for nums in num_list:
+        if nums > 0 and nums < 10:
+            print("The number is a single digit number.")
+        elif nums > 10 and nums < 100:
+            print("The number is a two digit number.")
+        elif nums > 100 and nums < 1000:
+            print("The number is a three digit number.")
+        else:
+            print(f"The number is {nums}")
+
 def main():
-    usr_num = get_usrinput()
+    list_of_num = []
+    list_of_num = get_usrinput()
+    do_max_min_num(list_of_num)
+    do_digit_check(list_of_num)
 
 
 if __name__ == "__main__":
